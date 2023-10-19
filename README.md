5fac88688f39adc5b8cd4d59ba8a0e92805aadb4
Hemant Singh <hsingh@impact-guard.com>
2023-10-19 09:07:57 -0400

    added api data

--- README.md
+++ README.md
@@ -1,6 +1,15 @@
 # pyInventory
 An inventory system developed in flask hosted in microsoft azure cloud
 
+## Setup For deployment
+1. Create  a resource group and an app service plan.
+2. Make sure you the requirments.txt, or else you miss dependencies
+3. In the APP Service plan make sure the configuration - general settings - 
+    gunicorn --bind=0.0.0.0 --timeout 600 startup:app  
+4. 
+
+## Deployment
+
 ## libraries
 py -m venv env
  py -m pip install --upgrade pip      
--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, jsonify
+import pandas as pd
 
 app = Flask(__name__)
 
@@ -7,5 +8,12 @@ def home():
     return "Welcome to Flask Home Page!"
 
 @app.route("/home")
-def index():
+def index():   
     return render_template("index.html")
+
+@app.route("/api/data")
+def getExtruderData():
+    filepath = 'app//data//extruderData.json'    
+    df = pd.read_json(filepath)
+    json_data = df.to_json(orient='records')
+    return json_data
