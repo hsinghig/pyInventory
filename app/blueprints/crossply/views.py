85e42553d9cdd2f5f52d40d6fb2ea80d0a2693ed
Hemant Singh <hsingh@impact-guard.com>
2023-11-30 22:28:10 -0500

    crossply menu working

--- app/blueprints/crossply/views.py
+++ app/blueprints/crossply/views.py
@@ -6,3 +6,7 @@ crossply_blueprint = Blueprint("crossply", __name__, template_folder="templates"
 @crossply_blueprint.route("/")
 def home():
     return render_template('xply.html')
+
+@crossply_blueprint.route("/add")
+def addcrossply():
+    return render_template('addCrossply.html')
